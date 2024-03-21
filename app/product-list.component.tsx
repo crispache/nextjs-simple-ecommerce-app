841fa3b75e0436baff60b5c184ab46c8f68c1be7
crispache <cristinaglezpache06@gmail.com>
2024-03-21 10:45:35 +0000

    Create component for product item

--- app/product-list.component.tsx
+++ app/product-list.component.tsx
@@ -1,6 +1,6 @@
-import { Product } from "./product-list.vm";
 import styles from "./product-list.module.css";
-import Image from "next/image";
+import { ProductItem } from "./product-item.component";
+import { Product } from "./product-list.vm";
 
 interface Props {
   products: Product[];
@@ -13,16 +13,7 @@ export const ProductList: React.FC<Props> = (props) => {
       <ul className={styles.productList}>
         {products.map((product) => (
           <li key={product.id} className={styles.product}>
-            <h3 className={styles.productName}> {product.name} </h3>
-            <p className={styles.productPrice}> {product.price} </p>
-            <button className={styles.productAddToCartButton}>
-              <Image
-                src="/icons/add-to-cart.png"
-                width={50}
-                height={28}
-                alt="Imagen del producto"
-              />
-            </button>
+            <ProductItem product={product} />
           </li>
         ))}
       </ul>
