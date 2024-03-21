8a38fdbac49eaea082b0a0d6b802188f6505ef93
crispache <cristinaglezpache06@gmail.com>
2024-03-21 00:44:46 +0000

    Create basic list design

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-
 import styles from "./page.module.css";
+import { mockProducts } from "@/app/_mock/products";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
-    <h1> Content </h1>
- 
+      <article>
+        <ul className={styles.productList}>
+          {mockProducts.map((product) => (
+            <li key={product.id} className={styles.product}>
+              <h3 className={styles.productName}> {product.name} </h3>
+              <p className={styles.productPrice}> {product.price} </p>
+              <button className={styles.productAddToCartButton}>
+                <Image 
+                  src="/icons/add-to-cart.png" 
+                  width={50}
+                  height={28}
+                  alt="Imagen del producto" 
+                  />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </article>
     </main>
   );
 }
